3f1129c640eb717df103ccd3c3eba9fa28300ca0
Sandeep <sdpk24@gmail.com>
2020-12-03 11:51:00 +0530

    eogie

--- src/Counters.js
+++ src/Counters.js
@@ -1,34 +1,24 @@
-import "./App.css";
-import { Slider } from "antd";
 import React, { useState } from "react";
+import { Slider } from "antd";
 
-function App() {
-  const [plus, setPlus] = useState([1]);
+const Counters = () => {
   const [size, setSize] = useState(10);
   const [radius, setRadius] = useState();
-
-  const increment = (val) => setPlus(plus.concat(val).slice(0, val));
-
   return (
     <div className="App">
       <div className="p-4 justify-between items-center width-58">
         <Slider className="w-40" min={200} max={300} onChange={setSize} />
         <Slider className="w-40 " min={30} max={100} onChange={setRadius} />
-        <Slider className="w-40 " min={1} max={100} onChange={increment} />
       </div>
-      <div>
-        {plus.map((val) => (
-          <div
-            style={{
-              height: size,
-              width: size,
-              borderRadius: radius,
-            }}
-            className="py-8 bg-gray-800 text-center m-10 text-white text-lg"
-          >
-            Hello
-          </div>
-        ))}
+      <div
+        style={{
+          height: size,
+          width: size,
+          borderRadius: radius,
+        }}
+        className="py-8 bg-gray-800 text-center m-10 text-white text-lg"
+      >
+        Hello
       </div>
       <div
         placeholder="Hello"
@@ -39,6 +29,6 @@ function App() {
       </div>
     </div>
   );
-}
+};
 
-export default App;
+export default Counters;
